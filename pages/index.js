fd454e185ea2a85cb0f34ee0a492182895ccb8b1
PauloBrunelli <paulinhoaugusto01@outlook.com>
2024-12-24 13:53:18 +0000

    adciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-function Home(){
-    return <h1>Estou alterando o index e treinando o Git!!!</h1>
+function Home() {
+  return <h1>Estou alterando o index e treinando o Git!!!</h1>;
 }
-
 export default Home;
